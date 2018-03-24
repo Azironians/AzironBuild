c7be11d15ab6611957e0af689371944cf9488dac
StarKRE <ardar.igor@mail.ru>
2018-03-24 13:32:58 +0300

    Working with bonuses

---
+++ src/managment/actionManagement/service/engine/services/DynamicHandleService.java
@@ -0,0 +1,9 @@
+package managment.actionManagement.service.engine.services;
+
+import managment.actionManagement.service.components.handleComponet.HandleComponent;
+
+public interface DynamicHandleService {
+
+    HandleComponent getHandlerInstance();
+
+}
